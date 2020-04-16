5e3560f00f45b6d25f97ec495eba6a8f5e85fb57
John IV <john.r.hoopes@gmail.com>
2020-04-16 17:27:15 +0100

    Setting up directory for D3 overlay tutorial code

---
+++ web-development/d3-overlay/js/overlay.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+var svg = d3.select(map.getPanes().overlayPane).append("svg");
+
+// fetch in data - ideally a points, lines and polygons layer. features API? 
+
+// Draw on SVG
+
+// Add some interactivity 
