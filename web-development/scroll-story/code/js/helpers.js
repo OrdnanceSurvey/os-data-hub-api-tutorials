213739e5683f5e17f3551dd9645b495d321342fb
John IV <john.iv@x25bd.com>
2020-06-25 12:57:13 +0100

    Update iframe; run all code through prettier

--- web-development/scroll-story/code/js/helpers.js
+++ web-development/scroll-story/code/js/helpers.js
@@ -1,32 +1,26 @@
 var layerTypes = {
-    'fill': ['fill-opacity'],
-    'line': ['line-opacity'],
-    'circle': ['circle-opacity', 'circle-stroke-opacity'],
-    'symbol': ['icon-opacity', 'text-opacity'],
-    'raster': ['raster-opacity'],
-    'fill-extrusion': ['fill-extrusion-opacity']
-}
+  fill: ["fill-opacity"],
+  line: ["line-opacity"],
+  circle: ["circle-opacity", "circle-stroke-opacity"],
+  symbol: ["icon-opacity", "text-opacity"],
+  raster: ["raster-opacity"],
+  "fill-extrusion": ["fill-extrusion-opacity"],
+};
 
-   
 var alignments = {
-    'left': 'lefty',
-    'center': 'centered',
-    'right': 'righty'
-}
-
-
-
-
+  left: "lefty",
+  center: "centered",
+  right: "righty",
+};
 
 function getLayerPaintType(layer) {
-    var layerType = map.getLayer(layer).type;
-    return layerTypes[layerType];
+  var layerType = map.getLayer(layer).type;
+  return layerTypes[layerType];
 }
 
-
 function setLayerOpacity(layer) {
-    var paintProps = getLayerPaintType(layer.layer);
-    paintProps.forEach(function (prop) {
-        map.setPaintProperty(layer.layer, prop, layer.opacity);
-    });
+  var paintProps = getLayerPaintType(layer.layer);
+  paintProps.forEach(function (prop) {
+    map.setPaintProperty(layer.layer, prop, layer.opacity);
+  });
 }
